81b5408cf75236808c08e40c5e51ad682889288b
Jan David <jandavid@6a64.com>
2019-06-10 19:29:14 +0200

    Add Actix
    Actix has been added back as a dependency of this project, and a simple
    server has been configured. The server does not yet serve any routes,
    and simply returns 404 for every request.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,19 @@
+use actix_web::{middleware, App, HttpServer};
 use dotenv::dotenv;
+use venja::config::Config;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init();
+
+    let config = Config::default();
+
+    HttpServer::new(|| {
+        App::new()
+            // Logger middleware must be wrapped last
+            .wrap(middleware::Logger::default())
+    })
+    .bind(config.server_address())
+    .expect("Failed to bind server address")
+    .run()
 }
